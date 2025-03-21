4671aec61b6a77cfdcc2ec325ed5454de31da9b7
Srinath Murali <srinathmurali2019@gmail.com>
2025-03-21 18:08:19 -0500

    Time: 916 ms (66.77%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1837-daily-leads-and-partners/daily-leads-and-partners.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+select date_id,make_name,COUNT(distinct lead_id) as unique_leads,COUNT(distinct partner_id) as unique_partners  from DailySales 
+group by date_id,make_name
