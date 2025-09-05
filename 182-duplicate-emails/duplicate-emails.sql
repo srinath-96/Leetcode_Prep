d7e3b3b19d45d1b39a7fc0c00820ce3be69bc36c
Srinath Murali <srinathmurali2019@gmail.com>
2025-09-05 10:54:38 +0530

    Time: 406 ms (26.43%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,4 +1,3 @@
 # Write your MySQL query statement below
-select email from (select id,email,Count(email) as a2 from Person
-group by email) a 
-where a2>1
+select email from (select email, Count(email) as c from Person group by email) a
+where a.c>1
